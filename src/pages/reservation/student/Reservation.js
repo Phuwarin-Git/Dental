a28ec91c4ec32f1a8877960038612e8fc3fd22ba
PHUWARIN MANEERAT <6131305047@lamduan.mfu.ac.th>
2021-08-18 00:33:09 +0700

    test

--- src/pages/reservation/student/Reservation.js
+++ src/pages/reservation/student/Reservation.js
@@ -167,7 +167,7 @@ const StudentRes = () => {
                 ) : null} <br />
 
 
-                <br /><button className="But" type="submit">Submit 1</button>
+                <br /><button className="But" type="submit">Submit</button>
             </form>
 
         </div>
