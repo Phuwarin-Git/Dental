781093e0f766ecae4a25ff14c0ea1df8acdc7e49
FlukeKroekphon <6131305002@lamduan.mfu.ac.th>
2021-09-06 23:15:07 +0700

    update

--- src/pages/equipment/student/tooth.js
+++ src/pages/equipment/student/tooth.js
@@ -7,11 +7,21 @@ import { Link } from "react-router-dom";
 import { AuthContext } from '../../../App';
 import axios from "axios";
 const equipmentdashboard = () => {
-    
-    
+
     return (
         <div>
-            
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/StudentDashboard">Dashboard</Nav.Link>
+                        <Nav.Link as={Link} to="/StudentRes">Reservation</Nav.Link>
+                        <Nav.Link as={Link} to="/StudentHistory">History</Nav.Link>
+                        <Nav.Link as={Link} to="/StudentProfile">Profile</Nav.Link>
+                        <Nav.Link as={Link} to="/ToothPage">Tooth</Nav.Link>
+                        <Nav.Link as={Link} to="/">Logout</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </div>
     )
 }
