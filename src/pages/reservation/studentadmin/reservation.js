e43e5051b07608176a572a2bb4af0921f77251ba
PHUWARIN MANEERAT <6131305047@lamduan.mfu.ac.th>
2021-09-10 22:20:41 +0700

    Add name to student admin select unit

--- src/pages/reservation/studentadmin/reservation.js
+++ src/pages/reservation/studentadmin/reservation.js
@@ -82,6 +82,7 @@ const StudentAdminReservation = () => {
 
                             <Card.Text>
                                 <Card.Title>Clinic : {item.clinic} &nbsp;&nbsp; เวลา : {item.time} </Card.Title>
+                                <lable>นักศึกษา : {item.name}</lable><br />
                                 <lable>ประเภทงาน : {item.worktype}</lable><br />
                                 <lable>คนไข้ : {item.patient}</lable><br />
 
