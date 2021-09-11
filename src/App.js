0374aec0eabe59b51db7c8986cfc1be60f0f90a8
PHUWARIN MANEERAT <6131305047@lamduan.mfu.ac.th>
2021-09-11 21:58:18 +0700

    Show listcases role student admin

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import React, { createContext, useEffect, useState } from 'react';
 
@@ -22,8 +22,9 @@ const AuthContext = createContext();
 function App() {
   const [loginStatus, setLoginStatus] = useState(false);
   const [user, setUser] = useState([]);
+
   useEffect(() => {
-    console.log('User :', user)
+    console.log('User :', user, ' Status :', loginStatus);
   }, [user])
   return (
     <div className="App">
@@ -31,21 +32,21 @@ function App() {
       <AuthContext.Provider value={{ user, setUser, loginStatus, setLoginStatus }}>
         <Switch>
           <Route exact path="/" component={Login}></Route>
-          <Route path="/StudentDashboard" component={StudentDashboard}></Route>
-          <Route path="/StudentRes" component={StudentRes}></Route>
-          <Route path="/StudentLimt" component={StudentLimt}></Route>
-          <Route path="/StudentHistory" component={StudentHistory}></Route>
-          <Route path="/StudentProfile" component={StudentProfile}></Route>
-          <Route path="/StudentAdminDashboard" component={StudentAdminDashboard}></Route>
-          <Route path="/StudentAdminReservation" component={StudentAdminReservation}></Route>
-          <Route path="/StudentAdminHistory" component={StudentAdminHistory}></Route>
-          <Route path="/StudentAdminLimitCase" component={StudentAdminLimitCase}></Route>
+          <Route path="/StudentDashboard" render={() => loginStatus === true ? <StudentDashboard /> : (<Redirect to="/" />)}></Route>
+          <Route path="/StudentRes" render={() => loginStatus === true ? <StudentRes /> : (<Redirect to="/" />)}></Route>
+          <Route path="/StudentLimt" render={() => loginStatus === true ? <StudentLimt /> : (<Redirect to="/" />)}></Route>
+          <Route path="/StudentHistory" render={() => loginStatus === true ? <StudentHistory /> : (<Redirect to="/" />)}></Route>
+          <Route path="/StudentProfile" render={() => loginStatus === true ? <StudentProfile /> : (<Redirect to="/" />)}></Route>
+          <Route path="/StudentAdminDashboard" render={() => loginStatus === true ? <StudentAdminDashboard /> : (<Redirect to="/" />)}></Route>
+          <Route path="/StudentAdminReservation" render={() => loginStatus === true ? <StudentAdminReservation /> : (<Redirect to="/" />)}></Route>
+          <Route path="/StudentAdminHistory" render={() => loginStatus === true ? <StudentAdminHistory /> : (<Redirect to="/" />)}></Route>
+          <Route path="/StudentAdminLimitCase" render={() => loginStatus === true ? <StudentAdminLimitCase /> : (<Redirect to="/" />)}></Route>
           <Route path="/:id">
-            <p>Error 404 page not found</p>
+            <h1 style={{ color: 'black' }}>Error 404 page not found</h1>
           </Route>
         </Switch>
       </AuthContext.Provider>
-    </div>
+    </div >
   );
 }
 export { AuthContext };
--- src/pages/reservation/studentadmin/dashboard.js
+++ src/pages/reservation/studentadmin/dashboard.js
@@ -1,10 +1,27 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import { Nav, Container } from 'react-bootstrap';
+import Table from 'react-bootstrap/Table'
 import { Link } from "react-router-dom";
 import { AuthContext } from '../../../App';
+import axios from "axios";
 const StudentAdminDashboard = () => {
     const { user } = useContext(AuthContext);
+    const [limit, setLimit] = useState([]);
+
+    useEffect(() => {
+        getDetails();
+        console.log("User :", user)
+    }, [user])
+
+    const getDetails = () => {
+        axios.get("http://selab.mfu.ac.th:8318/limitcase/find/all").then((item) => {
+            console.log("Limit :", item.data)
+            return setLimit(item.data);
+        });
+    }
+
+
     return (
         <div>
             <Navbar bg="dark" variant="dark">
@@ -20,8 +37,46 @@ const StudentAdminDashboard = () => {
                 </Container>
             </Navbar>
             <br />
-            <h1>Student Admin dashboard</h1>
-        </div>
+            <h1>Student Limit</h1>
+            <Table striped bordered hover variant="dark" style={{ marginLeft: 'auto', marginRight: 'auto', color: 'pink', maxWidth: '97%' }}>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>OD</th>
+                        <th>TMD</th>
+                        <th>OPER</th>
+                        <th>PERIO</th>
+                        <th>SUR</th>
+                        <th>RPOSTH</th>
+                        <th>ENDO</th>
+                        <th>X-RAY</th>
+                        <th>OM</th>
+                        <th>ORTHO</th>
+                    </tr>
+                </thead>
+                {limit.map(item => {
+                    return <tbody key={item.id}>
+                        <tr>
+                            <td style={{ color: 'white' }}>{item.date}</td>
+                            <td style={{ color: 'white' }}>{item.time}</td>
+                            <td style={{ color: 'white' }}>{item.od}</td>
+                            <td style={{ color: 'white' }}>{item.tmd}</td>
+                            <td style={{ color: 'white' }}>{item.oper}</td>
+                            <td style={{ color: 'white' }}>{item.perio}</td>
+                            <td style={{ color: 'white' }}>{item.sur}</td>
+                            <td style={{ color: 'white' }}>{item.prosth}</td>
+                            <td style={{ color: 'white' }}>{item.endo}</td>
+                            <td style={{ color: 'white' }}>{item.xray}</td>
+                            <td style={{ color: 'white' }}>{item.om}</td>
+                            <td style={{ color: 'white' }}>{item.ortho}</td>
+                        </tr>
+                    </tbody>
+
+                })}
+
+            </Table>
+        </div >
     )
 }
 export default StudentAdminDashboard;
