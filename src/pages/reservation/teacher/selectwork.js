25e47f13682d78fd54a5db7976a8d2727fe1bcf9
PHUWARIN MANEERAT <6131305047@lamduan.mfu.ac.th>
2021-09-25 17:35:27 +0700

    Update Teacher role

--- src/pages/reservation/teacher/selectwork.js
+++ src/pages/reservation/teacher/selectwork.js
@@ -3,6 +3,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import Navbar from 'react-bootstrap/Navbar'
+import Table from 'react-bootstrap/Table'
 import { Nav, Container } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -12,7 +13,7 @@ import '../Yup.css'
 
 const TeacherSelectWork = () => {
     const { user } = useContext(AuthContext);
-    const [limit, setLimit] = useState([]);
+    const [details, setDetails] = useState([]);
 
     useEffect(() => {
         getDetails();
@@ -20,12 +21,19 @@ const TeacherSelectWork = () => {
     }, [user])
 
     const getDetails = () => {
-        axios.get("http://selab.mfu.ac.th:8318/limitcase/find/all").then((item) => {
+        axios.get("http://selab.mfu.ac.th:8318/details/find/notnull").then((item) => {
             console.log("Limit :", item.data)
-            return setLimit(item.data);
+            return setDetails(item.data);
         });
     }
 
+    function submitReject() {
+        return alert("ปฏิเสธแล้ว")
+    }
+
+    function submitApprove() {
+        return alert("เลือกสำเร็จ")
+    }
 
     return (
         <div>
@@ -43,9 +51,39 @@ const TeacherSelectWork = () => {
             </Navbar>
             <br />
             <h1>Work Selection</h1>
+            <Table striped bordered hover variant="dark" style={{ marginLeft: 'auto', marginRight: 'auto', color: 'pink', maxWidth: '97%' }}>
+                <thead>
+                    <tr>
+                        <th>Unit</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Name</th>
+                        <th>Year</th>
+                        <th>Clinic</th>
+                        <th>Worktype</th>
+                        <th>Reject</th>
+                        <th>Approve</th>
 
+                    </tr>
+                </thead>
+                {details.map(item => {
+                    return <tbody >
+                        <tr>
+                            <td style={{ color: 'white' }}>{item.unit}</td>
+                            <td style={{ color: 'white' }}>{item.date}</td>
+                            <td style={{ color: 'white' }}>{item.time}</td>
+                            <td style={{ color: 'white' }}>{item.name}</td>
+                            <td style={{ color: 'white' }}>{item.studentyear}</td>
+                            <td style={{ color: 'white' }}>{item.clinic}</td>
+                            <td style={{ color: 'white' }}>{item.worktype}</td>
+                            <td><button style={{ backgroundColor: '#e85702', borderRadius: '10px', width: '80px', height: '35px' }} onClick={() => submitReject()}>ปฏิเสธ</button></td>
+                            <td><button style={{ backgroundColor: '#00d60b', borderRadius: '10px', width: '80px', height: '35px' }} onClick={() => submitApprove()}>ยืนยัน</button></td>
+                        </tr>
+                    </tbody>
+                })}
+            </Table>
 
-        </div>
+        </div >
     )
 }
 export default TeacherSelectWork;
