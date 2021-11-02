d52a62f40274cf97ded7399a85beda6d607e2bc0
[oppopcc] <6131305046@lamduan.mfu.ac.th>
2021-11-02 22:39:36 +0800

    Excel upload button

--- src/pages/reservation/studentadmin/limit.js
+++ src/pages/reservation/studentadmin/limit.js
@@ -5,18 +5,32 @@ import axios from "axios";
 import { Button } from 'react-bootstrap';
 import { useHistory } from "react-router-dom";
 import FormInput from './updateForm';
+import * as XLSX from "xlsx";
 
 const Limit = () => {
     const history = useHistory();
     const { user } = useContext(AuthContext);
     const [limit, setLimit] = useState([]);
     const [editingIndex, setEditingIndex] = useState([]);
-
+    const [items, setItems] = useState([]);
 
     useEffect(() => {
         getDetails();
         console.log("User :", user)
     }, [user])
+    useEffect(() => {
+        console.log(items[0])
+    //   console.log(items.OD)
+    //   console.log(items.TMD)
+    //   console.log(items.OPER)
+    //   console.log(items.PERIO)
+    //   console.log(items.SUR)
+    //   console.log(items.RPOSTH)
+    //   console.log(items.ENDO)
+    //   console.log(items.XRAY)
+    //   console.log(items.OM)
+    //   console.log(items.ORTHO)
+    }, [items])
 
     const getDetails = () => {
         // http://selab.mfu.ac.th:8318/limitcase/find/all
@@ -46,11 +60,47 @@ const Limit = () => {
     function changeStatus(ID) {
         setEditingIndex([...editingIndex, ID])
     }
+    
+    const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(file);
+
+    fileReader.onload = (e) => {
+    const bufferArray = e.target.result;
+
+    const wb = XLSX.read(bufferArray, { type: "buffer" });
+
+    const wsname = wb.SheetNames[0];
+
+    const ws = wb.Sheets[wsname];
+
+    const data = XLSX.utils.sheet_to_json(ws);
+
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      setItems(d);
+      
+    });}
+    
 
 
     return (
         <div>
             <h1 style={{ color: '#0047AB', fontWeight: 'bold' }}>จำนวนภาระงาน</h1>
+            <input type="file"onChange={(e) => {
+                const file = e.target.files[0];
+                readExcel(file);
+                }}
+             />
+             
             <Table striped bordered hover variant="" style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: '97%' }}>
                 <thead className='theadAdmin'>
                     <tr>
