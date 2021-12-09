82a5b7ca435f27a730d309c5894ff735e97ad2fe
PHUWARIN MANEERAT <6131305047@lamduan.mfu.ac.th>
2021-12-10 06:09:00 +0700

    Reservation student admin table

--- src/pages/reservation/studentadmin/reservation.js
+++ src/pages/reservation/studentadmin/reservation.js
@@ -6,7 +6,9 @@ import { Link, useHistory } from "react-router-dom";
 import { AuthContext } from '../../../App';
 import axios from "axios";
 import Button from './reservationCss/ButtonRes'
-import { BsSearch } from "react-icons/bs";
+
+import MaterialTable from "material-table";
+
 const StudentAdminReservation = () => {
 
     const { user, currentDate, currentMonth } = useContext(AuthContext);
@@ -17,12 +19,19 @@ const StudentAdminReservation = () => {
     const [unitNotnull, setNotnull] = useState([]);
     const [unitByFloor, setUnitByFloor] = useState([]);
 
+    const [data, setData] = useState([]);
+
+
     useEffect(() => {
         getDetails();
         getUnit();
-        getUnitNotNull();
+        // getUnitNotNull();
         // console.log("Unit จองแล้ว :", unitNotnull)
     }, [user])
+
+    useEffect(() => {
+        console.log("Seleted :", select)
+    }, [select])
 
 
     const getDetails = () => {
@@ -48,6 +57,12 @@ const StudentAdminReservation = () => {
                 return (parsed >= currentMonth && digitRealDate >= thisDate)
             })
 
+            let filteredData = []
+            filterMonth.map(item => {
+                return filteredData.push({ id: item.id, uniqueID: item.uniqueID, name: item.name, year: item.studentyear, date: item.date, time: item.time, unit: item.unit, clinic: item.clinic, worktype: item.worktype, patient: item.patient, teacher: item.teacher, dn: item.dn, hn: item.hn, toolStatus: item.toolStatus })
+            })
+            setData(filteredData);
+
             return setDetails(filterMonth);
         });
     }
@@ -55,35 +70,35 @@ const StudentAdminReservation = () => {
     const getUnit = () => {
         axios.get("http://localhost:3000/unit/find/all").then((item) => {
             // console.log("Unit lists:", item.data)
-            setUnitByFloor(item.data);
+            // setUnitByFloor(item.data);
             return setAllUnit(item.data);
         });
     }
 
-    function getUnitNotNull() {
-        axios.get("http://localhost:3000/details/find/notnull").then((item) => {
-            console.log("Unit not null ==>", item.data)
-            let findMonth = item.data;
-            let filterMonth = findMonth.filter((item) => {
-                let a = item.date;
-                let thisDate = currentDate.slice(8)
-                let digitRealDate = (a).slice(8)
-                let digitData = (a).slice(5, 7)
-                let parsed = parseInt(digitData)
-                return (parsed >= currentMonth && digitRealDate >= thisDate)
-            })
-            return setNotnull(filterMonth);
-        });
-    }
+    // function getUnitNotNull() {
+    //     axios.get("http://localhost:3000/details/find/notnull").then((item) => {
+    //         console.log("Unit not null ==>", item.data)
+    //         let findMonth = item.data;
+    //         let filterMonth = findMonth.filter((item) => {
+    //             let a = item.date;
+    //             let thisDate = currentDate.slice(8)
+    //             let digitRealDate = (a).slice(8)
+    //             let digitData = (a).slice(5, 7)
+    //             let parsed = parseInt(digitData)
+    //             return (parsed >= currentMonth && digitRealDate >= thisDate)
+    //         })
+    //         return setNotnull(filterMonth);
+    //     });
+    // }
 
 
+    //unit
     function handleOnChange(e) {
         console.log('Value :', e.target.value.split(" "))
         let first = e.target.value.split(" ")
         // console.log('id :', first[0])
         // console.log('unit :', first[1])
         setSelect([...select, { id: first[0], unit: first[1] }]);
-        console.log('Seleted :', select)
     };
 
 
@@ -117,7 +132,7 @@ const StudentAdminReservation = () => {
                         let parsed = parseInt(digitData)
                         return (parsed >= currentMonth && digitRealDate >= thisDate)
                     })
-
+                    setData(filterMonth)
                     return setDetails(filterMonth);
                 });
             } else {
@@ -126,32 +141,32 @@ const StudentAdminReservation = () => {
         }
     }
 
-    async function onChangeSearch(e) {
-        await axios.get("http://localhost:3000/details/find/null").then((item) => {
-            console.log("new Limit ==> :", item.data)
-            let findMonth = item.data;
-            let filterMonth = findMonth.filter((item) => {
-                let a = item.date;
-                let thisDate = currentDate.slice(8)
-                let digitRealDate = (a).slice(8)
-                let digitData = (a).slice(5, 7)
-                let parsed = parseInt(digitData)
-                return (parsed >= currentMonth && digitRealDate >= thisDate)
-            })
-            return setDetails(filterMonth);
-        });
-        console.log("Change Date :", e.target.value)
-        setSearchDate(e.target.value)
-    }
+    // async function onChangeSearch(e) {
+    //     await axios.get("http://localhost:3000/details/find/null").then((item) => {
+    //         console.log("new Limit ==> :", item.data)
+    //         let findMonth = item.data;
+    //         let filterMonth = findMonth.filter((item) => {
+    //             let a = item.date;
+    //             let thisDate = currentDate.slice(8)
+    //             let digitRealDate = (a).slice(8)
+    //             let digitData = (a).slice(5, 7)
+    //             let parsed = parseInt(digitData)
+    //             return (parsed >= currentMonth && digitRealDate >= thisDate)
+    //         })
+    //         return setDetails(filterMonth);
+    //     });
+    //     console.log("Change Date :", e.target.value)
+    //     setSearchDate(e.target.value)
+    // }
 
-    function Searching() {
-        console.log("Searching :", searchDate)
-        const checking = details.filter((item) => {
-            return item.date === searchDate
-        })
-        console.log("Filter Date", checking)
-        setDetails(checking)
-    }
+    // function Searching() {
+    //     console.log("Searching :", searchDate)
+    //     const checking = details.filter((item) => {
+    //         return item.date === searchDate
+    //     })
+    //     console.log("Filter Date", checking)
+    //     setDetails(checking)
+    // }
 
     function filterUnit(date, time, unit) {
         // return console.log('Unit ==>', unit)
@@ -201,81 +216,156 @@ const StudentAdminReservation = () => {
             </Navbar>
 
             <br />
-            <Container style={{ backgroundColor: 'white', padding: '15px', borderRadius: '10px', minHeight: '700px', minWidth: '1500px' }}>
-                <h1 style={{ color: '#198CFF', fontWeight: 'bold' }}>การเลือกที่นั่ง</h1>
-                <label style={{ fontSize: '18px', fontWeight: 'bold', marginRight: '10px', marginLeft: '20px' }}>ค้นหาวันที่ : </label>
-                <input
-                    style={{ fontSize: '18px' }}
-                    type="date"
-                    min={currentDate}
-                    class="searchTerm"
-                    id="input_text"
-                    placeholder="ค้นหาวันที่"
-                    onChange={onChangeSearch}
-                >
-                </input>
-                <button onClick={() => Searching()} type="submit" class="searchButton">
-                    <BsSearch />
-                </button>
+            <div className="PaddingDiv">
+                <Container style={{ backgroundColor: 'white', padding: '15px', borderRadius: '10px', minHeight: '700px', maxWidth: '1500px' }}>
+                    <h1 style={{ color: '#198CFF', fontWeight: 'bold', marginBottom: '10px' }}>การเลือกที่นั่ง</h1>
+                    {/* <label style={{ fontSize: '18px', fontWeight: 'bold', marginRight: '10px', marginLeft: '20px' }}>ค้นหาวันที่ : </label>
+                    <input
+                        style={{ fontSize: '18px' }}
+                        type="date"
+                        min={currentDate}
+                        class="searchTerm"
+                        id="input_text"
+                        placeholder="ค้นหาวันที่"
+                        onChange={onChangeSearch}
+                    >
+                    </input>
+                    <button onClick={() => Searching()} type="submit" class="searchButton">
+                        <BsSearch />
+                    </button> */}
 
-                <Table striped bordered hover variant="" style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: '97%', marginTop: '20px' }}>
-                    <thead className='theadAdmin'>
-                        <tr>
-                            <th>วันที่</th>
-                            <th>ช่วงเวลา</th>
-                            <th>คลินิก</th>
-                            <th>ประเภทงาน</th>
-                            <th>ชื่อผู้ป่วย</th>
-                            <th>ชื่อนักศึกษา</th>
-                            <th>Unit</th>
-                        </tr>
-                    </thead>
-                    {details.map(item => {
+                    <MaterialTable
+                        title="Mae Fah Luang University Dental Clinic"
+                        columns={[
+                            {
+                                title: 'วันที่', field: 'date', cellStyle: {
+                                    minWidth: 140,
+                                },
+                            },
+                            {
+                                title: 'ช่วงเวลา', field: 'time', cellStyle: {
+                                    minWidth: 125,
+                                },
+                            },
+                            {
+                                title: 'คลินิก', field: 'clinic', cellStyle: {
+                                    minWidth: 30,
+                                },
+                            },
+                            {
+                                title: 'ประเภทงาน', field: 'worktype', cellStyle: {
+                                    minWidth: 145,
+                                },
+                            },
+                            {
+                                title: 'คนไข้', field: 'patient', cellStyle: {
+                                    minWidth: 220,
+                                },
+                            },
+                            {
+                                title: 'ชื่อนักศึกษา', field: 'name', cellStyle: {
+                                    minWidth: 220,
+                                },
+                            },
+                            {
+                                title: 'Unit', field: 'selectionUnit', cellStyle: {
+                                    minWidth: 50,
+                                }, editable: false,
+                                render: (rowData) =>
+                                    rowData && (
+                                        <select style={{ backgroundColor: '#198CFF', color: 'white', }} onChange={handleOnChange}>
+                                            <option value="selected" selected="selected">เลือก Unit</option>
+                                            {allUnit?.map(items => {
+                                                if (items.unavailable_start_date === 'active') {
+                                                    if (filterUnit(rowData.date, rowData.time, items.unit_code)) {
+                                                        return <option
+                                                            style={{ backgroundColor: '#c7c7c7', color: 'black' }}
+                                                            value={rowData.id + " " + items.unit_code} disabled>
+                                                            {items.unit_code}{" ถูกจองแล้ว"}
+                                                        </option>
+                                                    } else {
+                                                        return <option
+                                                            style={{ backgroundColor: 'white', color: 'black' }}
+                                                            value={rowData.id + " " + items.unit_code} >
+                                                            {items.unit_code}
+                                                        </option>
+                                                    }
 
-                        return <tbody key={item.id}>
-                            <tr>
-                                <td className='tdStudent'>{item.date}</td>
-                                <td className='tdStudent'>{item.time}</td>
-                                <td className='tdStudent'>{item.clinic}</td>
-                                <td className='tdStudent'>{item.worktype}</td>
-                                <td className='tdStudent'>{item.patient}</td>
-                                <td className='tdStudent'>{item.name}</td>
-                                <td className='tdStudent'>
-                                    <select style={{ backgroundColor: '#198CFF', color: 'white', width: '190px' }} onChange={handleOnChange}>
-                                        <option value="selected" selected="selected">เลือก Unit</option>
-                                        {allUnit?.map(items => {
-                                            if (items.unavailable_start_date === 'active') {
-                                                if (filterUnit(item.date, item.time, items.unit_code)) {
+                                                } else if (items.unavailable_start_date === 'inactive') {
                                                     return <option
                                                         style={{ backgroundColor: '#c7c7c7', color: 'black' }}
-                                                        value={item.id + " " + items.unit_code} disabled>
-                                                        {items.unit_code}{" ถูกจองแล้ว"}
-                                                    </option>
-                                                } else {
-                                                    return <option
-                                                        style={{ backgroundColor: 'white', color: 'black' }}
-                                                        value={item.id + " " + items.unit_code} >
-                                                        {items.unit_code}
+                                                        value={rowData.id + " " + rowData.unit_code} disabled>
+                                                        {items.unit_code}{" ปิดใช้งาน"}
                                                     </option>
                                                 }
-
-                                            } else if (items.unavailable_start_date === 'inactive') {
-                                                return <option
-                                                    style={{ backgroundColor: '#c7c7c7', color: 'black' }}
-                                                    value={item.id + " " + items.unit_code} disabled>
-                                                    {items.unit_code}{" ปิดใช้งาน"}
-                                                </option>
                                             }
-                                        }
-                                        )}
-                                    </select>
-                                </td>
-                            </tr>
-                        </tbody>
-                    })}
-                </Table>
-                <Button style={{ fontWeight: 'bold', backgroundColor: '#198CFF' }} onClick={() => submitApprove()}>ยืนยัน</Button>
-            </Container>
+                                            )}
+                                        </select>
+                                    )
+                            },
+
+                        ]}
+                        data={data}
+                        options={{
+                            actionsColumnIndex: -1,
+                            headerStyle: {
+                                fontFamily: "Mitr",
+                                fontWeight: 'bold',
+                                fontSize: '18px',
+                            }, tableLayout: 'auto'
+                        }}
+                        localization={{
+                            body: {
+                                emptyDataSourceMessage: 'ไม่มีการจอง Unit',
+                                addTooltip: 'Hinzufügen',
+                                deleteTooltip: 'Löschen',
+                                editTooltip: 'Bearbeiten',
+                                filterRow: {
+                                    filterTooltip: 'Filter'
+                                },
+                                editRow: {
+                                    deleteText: 'Diese Zeile wirklich löschen?',
+                                    cancelTooltip: 'Abbrechen',
+                                    saveTooltip: 'Speichern'
+                                }
+                            },
+                            grouping: {
+                                placeholder: 'Spalten ziehen ...',
+                                groupedBy: 'Gruppiert nach:'
+                            },
+                            header: {
+                                actions: 'รายละเอียดการจอง'
+                            },
+                            pagination: {
+                                labelDisplayedRows: '{from}-{to} จาก {count}',
+                                labelRowsSelect: 'แถว',
+                                labelRowsPerPage: 'Zeilen pro Seite:',
+                                firstAriaLabel: 'Erste Seite',
+                                firstTooltip: 'Erste Seite',
+                                previousAriaLabel: 'Vorherige Seite',
+                                previousTooltip: 'Vorherige Seite',
+                                nextAriaLabel: 'Nächste Seite',
+                                nextTooltip: 'Nächste Seite',
+                                lastAriaLabel: 'Letzte Seite',
+                                lastTooltip: 'Letzte Seite'
+                            },
+                            toolbar: {
+                                addRemoveColumns: 'Spalten hinzufügen oder löschen',
+                                nRowsSelected: '{0} Zeile(n) ausgewählt',
+                                showColumnsTitle: 'Zeige Spalten',
+                                showColumnsAriaLabel: 'Zeige Spalten',
+                                exportTitle: 'Export',
+                                exportAriaLabel: 'Export',
+                                exportName: 'Export als CSV',
+                                searchTooltip: 'ค้นหา',
+                                searchPlaceholder: 'ค้นหา'
+                            }
+                        }}
+                    />
+
+                    <Button style={{ fontWeight: 'bold', backgroundColor: '#198CFF', marginTop: '10px' }} onClick={() => submitApprove()}>ยืนยัน</Button>
+                </Container>
+            </div>
         </div>
     )
 }
